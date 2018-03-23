8eea888879a2d6a44461193d7d35ed9984e8b932
Your Name <xueqingc@gmail.com>
2018-03-23 15:28:31 -0700

    add detect network for Diner's Clue and American Express

--- detectNetwork.js
+++ detectNetwork.js
@@ -13,6 +13,13 @@ var detectNetwork = function(cardNumber) {
   // The American Express network always starts with a 34 or 37 and is 15 digits long
 
   // Once you've read this, go ahead and try to implement this function, then return to the console.
-};
-
-
+ if (cardNumber.length === 14) {
+   if (cardNumber.slice(0,2) === '38' || cardNumber.slice(0, 2) === '39'){
+    return "Diner\'s Club";
+   } 
+   if (cardNumber.length === 15) {
+    if (cardNumber.slice(0,2) === '34' || cardNumber.slice(0, 2) === '37'){
+     return "American Express";
+    }
+   }
+}
